3a371c8f5849819fd6e1a6880dc0cae73c8bc510
Subhranil Ghoshal <ghoshalsubhranil@gmail.com>
2024-08-26 14:55:33 +0530

    Updated header file to remove function definitions

--- registers.h
+++ registers.h
@@ -1,21 +1,21 @@
-#include <stdio.h>
 #include <iostream>
 #include "types.h"
 
 using namespace std;
 
+#ifndef REGISTERS_H
+#define REGISTERS_H
+
 class reg_8bit{
 	/* General structure of 8-bit registers in 8085 */
 	protected:
 		uint8_t	DATA	: 8;
-	
 	public:
 		//constructor
-		reg_8bit(uint8_t data) {DATA = data;}
+		reg_8bit(uint8_t data);
 
-		uint8_t getData() {return DATA;}
-		void setData(uint8_t newData) {DATA = newData;}
-
+		uint8_t getData();
+		void setData(uint8_t newData);
 };
 
 class GPR: public reg_8bit{
@@ -23,17 +23,15 @@ class GPR: public reg_8bit{
 	protected:
 		uint8_t CODE	: 3;
 		uint8_t			: 0;
-
 	public:
-		GPR(uint8_t code, uint8_t data = 0b0): reg_8bit(data) {CODE = code;}
+		GPR(uint8_t code, uint8_t data = 0b0);
 };
 typedef GPR Register;
 
 class Accumulator: public GPR{
 	/* Accumulator Register A */
 	public:
-		Accumulator(uint8_t data=0b0): GPR(0b111, data) {}
-		
+		Accumulator(uint8_t data=0b0);
 		//data transfer group functions
 		void stax();
 		void ldax();
@@ -58,7 +56,6 @@ class Accumulator: public GPR{
 		void xra(Register R, FlagRegister* F);
 		void ora(Register R, FlagRegister* F);
 		void cmp(Register R, FlagRegister* F);
-
 		void ani(uint8_t data, FlagRegister* F);
 		void xri(uint8_t data, FlagRegister* F);
 		void ori(uint8_t data, FlagRegister* F);
@@ -72,31 +69,17 @@ class Accumulator: public GPR{
 class FlagRegister: public reg_8bit{
 	/* Flag Register F */
 	public:
-		FlagRegister(uint8_t initialData = 0b0): reg_8bit(0b0) {setData(initialData);}
+		FlagRegister(uint8_t initialData = 0b0);
 
-		//applying bit mask to keep all non-flag values at zero
-		uint8_t getData() {return DATA & 0b11010101;}
-		void setData(uint8_t newData) {DATA = newData & 0b11010101;}
+		uint8_t getData();
+		void setData(uint8_t newData);
 		
-		void setCarry(bool set){
-			if(set) DATA |= 0b00000001;
-			else 	DATA &= 0b11111110;
-		}
-		void setParity(bool set) {
-			if(set)	DATA |= 0b00000100;
-			else	DATA &= 0b11111011;
-		}
-		void setAuxCarry(bool set) {
-			if(set)	DATA |= 0b00010000;
-			else	DATA &= 0b11101111;
-		}
-		void setZero(bool set) {
-			if(set)	DATA |= 0b01000000;
-			else	DATA &= 0b10111111;
-		}
-		void setSign(bool set) {
-			if(set)	DATA |= 0b10000000;
-			else	DATA &= 0b01111111;
-		}
+		void setCarry(bool set);
+		void setParity(bool set);
+		void setAuxCarry(bool set);
+		void setZero(bool set);
+		void setSign(bool set);
 		void resetFlags() {setData(0);}
 };
+
+#endif

11e74a4111ce5e4c291faf402c145d3baf77d266
Subhranil Ghoshal <ghoshalsubhranil@gmail.com>
2024-10-24 12:09:39 +0530

    Changed implementation and instruction descriptions

--- registers/gpr.cpp
+++ registers/gpr.cpp
@@ -1,17 +1,19 @@
 #include "registers.h"
 
+/*Constructors*/
 reg_8bit::reg_8bit(uint8_t data) {
 	DATA = data;
 }
 
+GPR::GPR(uint8_t code, uint8_t data = 0b0): reg_8bit(data) {
+	CODE = code;
+}
+
+/*Internal functions*/
 uint8_t reg_8bit::getData() {
 	return DATA;
 }
 
 void reg_8bit::setData(uint8_t newData) {
 	DATA = newData;
-}
-
-GPR::GPR(uint8_t code, uint8_t data = 0b0): reg_8bit(data) {
-	CODE = code;
 }
